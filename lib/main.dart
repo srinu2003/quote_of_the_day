ff996da49b7543ec5f1bee9b92ef3227c3e73090
srinu2003 <tsrin2003@gmail.com>
2024-06-15 22:16:22 +0530

    chore: Update flutter_lints package to version 4.0.0 and add dynamic_color and google_fonts dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:google_fonts/google_fonts.dart';
+
+/// Flutter code sample for [Quote_of_the_day].
 
 void main() {
-  runApp(const MainApp());
+  runApp(const QuotesApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class QuotesApp extends StatelessWidget {
+  const QuotesApp({super.key});
 
+  // This widget is the root of my application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      title: 'Quote of the Day',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          // This is the theme of my application.
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          useMaterial3: true),
+      home: const Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('The Quote of the Day app!'),
         ),
       ),
     );
