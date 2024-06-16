ac9e128edb959bdc013db7013617ba520b0a3603
srinu2003 <tsrin2003@gmail.com>
2024-06-16 13:38:56 +0530

    chore: Update QuoteOfTheDay widget with conditional rendering of FloatingActionButton
    This commit updates the QuoteOfTheDay widget to conditionally render the FloatingActionButton based on the currentPageIndex. If the currentPageIndex is 0, the FloatingActionButton with the label "Inspire me" is displayed, allowing users to refresh the quote. This change improves the user experience by providing a relevant action button only when needed.

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,8 @@ class QuoteOfTheDay extends StatefulWidget {
 class _QuoteOfTheDayState extends State<QuoteOfTheDay> {
   int currentPageIndex = 0;
   bool? saveSelected = false;
+  // ignore: unused_field
+  final bool _enableFAB = false;
 
   // ignore: unused_element
   void _changeQuote() {
@@ -88,15 +90,17 @@ class _QuoteOfTheDayState extends State<QuoteOfTheDay> {
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        label: const Text('Inspire me'), // for Extended FAB
-        backgroundColor: theme.tertiaryContainer,
-        foregroundColor: theme.onTertiaryContainer,
-        onPressed: _changeQuote,
-        tooltip: 'Refresh',
-        icon: const Icon(Icons.auto_awesome_rounded), // for Extended FAB
-        // child: const Icon(Icons.refresh_rounded),
-      ),
+      floatingActionButton: currentPageIndex == 0
+          ? FloatingActionButton.extended(
+              label: const Text('Inspire me'), // for Extended FAB
+              backgroundColor: theme.tertiaryContainer,
+              foregroundColor: theme.onTertiaryContainer,
+              onPressed: _changeQuote,
+              tooltip: 'Refresh',
+              icon: const Icon(Icons.auto_awesome_rounded), // for Extended FAB
+              // child: const Icon(Icons.refresh_rounded),
+            )
+          : null,
       body: <Widget>[
         /// Quote of the day Home Page
 
